72bb2bd2bf9730361461ddd964e8ec6ba478503f
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2024-02-09 07:51:33 -0800

    thumbs up emoji

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -7,35 +7,172 @@ namespace Vector_Calculator
         static void Main(string[] args)
         {
             string x1;
-            string x2;
+            string x2; 
             string y1;
             string y2;
             string z1;
             string z2;
-            Console.WriteLine("Please input an x value for the first vector.");
+            Console.WriteLine("Please input an integer within the 32 bit limit as an x value for the first vector.");
             x1 = Console.ReadLine();
-            Console.WriteLine("Please input an x value for the second vector.");
-            x2 = Console.ReadLine();
-            Console.WriteLine("Please input an y value for the first vector.");
+            Console.Clear();
+            Console.WriteLine("Please input an integer within the 32 bit limit as a y value for the first vector.");
             y1 = Console.ReadLine();
-            Int16.Parse(y1);
-            Console.WriteLine("Please input an y value for the second vector.");
-            y2 = Console.ReadLine();
-            Int16.Parse(y2);
-            Console.WriteLine("Please input an z value for the first vector.");
+            Console.Clear();
+            Console.WriteLine("Please input an integer within the 32 bit limit as a z value for the first vector.");
             z1 = Console.ReadLine();
-            Int16.Parse(z1);
-            Console.WriteLine("Please input an z value for the second vector.");
+            Console.Clear();
+            Console.WriteLine("Please input an integer within the 32 bit limit as an x value for the second vector.");
+            x2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please input an integer within the 32 bit limit as a y value for the second vector.");
+            y2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please input an integer within the 32 bit limit as a z value for the second vector.");
             z2 = Console.ReadLine();
-            Int16.Parse(z2);
-            Vector test1 = new Vector(Int16.Parse(x1), Int16.Parse(y1), Int16.Parse(z1));
-            Vector test2 = new Vector(Int16.Parse(x2), Int16.Parse(y2), Int16.Parse(z2));
+            Console.Clear();
+            Vector test1 = new Vector(Int32.Parse(x1), Int32.Parse(y1), Int32.Parse(z1));
+            Vector test2 = new Vector(Int32.Parse(x2), Int32.Parse(y2), Int32.Parse(z2));
             Console.WriteLine("What function would you like to do with these vector(s)?");
-            Console.WriteLine("ADD || SUBTRACT || NEGATE");
+            Console.WriteLine("ADD || SUBTRACT || NEGATE || SCALE || MAGNITUDE || NORMALIZE || DOTPRODUCT || CROSSPRODUCT || ANGLEBETWEEN || GETDIRECTION || PROJECTONTO");
             string userInput = Console.ReadLine();
-            if (userInput == "add")
+            Console.Clear();
+            if (userInput.ToLower() == "add")
             {
-                Vector Add(test1, test2)
+                Console.WriteLine(test1 + " + " + test2 + " = ");
+                Console.WriteLine(Vector.Add(test1, test2));
+            }
+            else if (userInput.ToLower() == "subtract")
+            {
+                Console.WriteLine(test1 + " - " + test2 + " = ");
+                Console.WriteLine(Vector.Subtract(test1, test2));
+            }
+            else if (userInput.ToLower() == "negate")
+            {
+                Console.WriteLine("Which vector would you like to negate?");
+                Console.WriteLine("Vector1: " + test1 + "     Vector2: " + test2);
+                userInput  = Console.ReadLine();
+                if (userInput.ToLower() == "vector1" || userInput == "1")
+                {
+                    Console.WriteLine(Vector.Negate(test1));
+                }
+                else if (userInput.ToLower() == "vector2" || userInput == "2")
+                {
+                    Console.WriteLine(Vector.Negate(test2));
+                }
+                
+            }
+            else if (userInput.ToLower() == "scale")
+            {
+                Console.WriteLine("Which vector would you like to scale?");
+                Console.WriteLine("Vector1: " + test1 + "     Vector2: " + test2);
+                userInput  = Console.ReadLine();
+                Console.Clear();
+                if (userInput.ToLower() == "vector1" || userInput == "1")
+                {
+                    Console.WriteLine("What integer would you like to scale it by?");
+                    int scaleValue = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine(Vector.Scale(test1, scaleValue));
+                }
+                else if (userInput.ToLower() == "vector2" || userInput == "2")
+                {
+                    Console.WriteLine("What integer would you like to scale it by?");
+                    int scaleValue = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine(Vector.Scale(test2, scaleValue));
+                }
+                
+            }
+            else if (userInput.ToLower() == "magnitude")
+            {
+                Console.WriteLine("Which vector would you like to get the magnitude of?");
+                Console.WriteLine("Vector1: " + test1 + "     Vector2: " + test2);
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() == "vector1" || userInput == "1")
+                {
+                    Console.WriteLine(test1.GetMagnitude());
+                }
+                else if (userInput.ToLower() == "vector2" || userInput == "2")
+                {
+                    Console.WriteLine(test2.GetMagnitude());
+                }
+            }
+            else if (userInput.ToLower() == "normalize")
+            {
+                Console.WriteLine("Which vector would you like to normalize?");
+                Console.WriteLine("Vector1: " + test1 + "     Vector2: " + test2);
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() == "vector1" || userInput == "1")
+                {
+                    Console.WriteLine(Vector.Normalize(test1));
+                }
+                else if (userInput.ToLower() == "vector2" || userInput == "2")
+                {
+                    Console.WriteLine(Vector.Normalize(test2));
+                }
+            }
+            else if (userInput.ToLower() == "dotproduct")
+            {
+                Console.WriteLine(test1 + " • " + test2 + " = ");
+                Console.WriteLine(Vector.DotProduct(test1, test2));
+            }
+            else if (userInput.ToLower() == "crossproduct")
+            {
+                Console.WriteLine(test1 + " X " + test2 + " = ");
+                Console.WriteLine(Vector.CrossProduct(test1, test2));
+            }
+            else if (userInput.ToLower() == "anglebetween")
+            {
+                Console.WriteLine("The angle between " + test1 + " and " + test2 + " is:");
+                Console.WriteLine(Vector.AngleBetween(test1, test2));
+            }
+            else if (userInput.ToLower() == "getdirection")
+            {
+                Console.WriteLine("Which vector would you like to get the direction of?");
+                Console.WriteLine("Vector1: " + test1 + "     Vector2: " + test2);
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() == "vector1" || userInput == "1")
+                {
+                    Console.WriteLine(test1.GetDirection());
+                }
+                else if (userInput.ToLower() == "vector2" || userInput == "2")
+                {
+                    Console.WriteLine(test2.GetDirection());
+                }
+            }
+            else if (userInput.ToLower() == "projectonto")
+            {
+                Console.WriteLine(test1 + " projected onto " + test2 + " is:");
+                Console.WriteLine(Vector.ProjectOnto(test1, test2));
+            }
+            else if (userInput.ToLower() == "derek")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hello Derek :)");
+                userInput = Console.ReadLine();
+                Console.Clear();
+                if (userInput.ToLower() == "derek?")
+                {
+                    Console.WriteLine("DEREK >:)");
+                    System.Threading.Thread.Sleep(500);
+                    Console.Clear();
+                    System.Threading.Thread.Sleep(500);
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine("/////////////////////////////////////////////////////////////////////////////////////////");
+                }
             }
             
         }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -25,68 +25,64 @@ namespace Vector_Calculator
 
         public float GetMagnitude()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return (float)Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2));
         }
 
         public float GetDirection()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return (float)Math.Atan(this.y / this.x);
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
             return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
         public static Vector Negate(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
             return new Vector(v.x * -1, v.y * -1, v.z * -1);
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
             return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
-        public static Vector Scale(Vector v)
+        public static Vector Scale(Vector v, int x)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v.x * x, v.y * x, v.z * x);
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            float mag = (float) Math.Sqrt(Math.Pow(v.x, 2) + Math.Pow(v.y, 2) + Math.Pow(v.z, 2));
+            return new Vector(v.x / mag, v.y / mag, v.z / mag);
         }
 
         public static float DotProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return (float)((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
         }
 
         public static Vector CrossProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector((v1.y * v2.z) - (v1.z * v2.y), (v1.z * v2.x) - (v1.x * v2.z), (v1.x * v2.y) - (v1.y * v2.x));
         }
 
-        public static Vector AngleBetween(Vector v1, Vector v2)
+        public static float AngleBetween(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            float mag1 = (float)Math.Sqrt(Math.Pow(v1.x, 2) + Math.Pow(v1.y, 2) + Math.Pow(v1.z, 2));
+            float mag2 = (float)Math.Sqrt(Math.Pow(v2.x, 2) + Math.Pow(v2.y, 2) + Math.Pow(v2.z, 2));
+            float dot = ((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
+            return (float)(Math.Acos((dot / (mag1 * mag2))) * (180 / Math.PI));
         }
 
         public static Vector ProjectOnto(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            float dot = ((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
+            float mag2 = (float)Math.Sqrt(Math.Pow(v2.x, 2) + Math.Pow(v2.y, 2) + Math.Pow(v2.z, 2));
+            float projNum = (dot) / (mag2*mag2);
+            return new Vector((v2.x * projNum), (v2.y * projNum), (v2.z * projNum));
         }
     }
 }
